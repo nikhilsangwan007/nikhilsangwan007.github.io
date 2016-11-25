4eead91766a2eab91a896835afb8ee240d2b9fff
nikhilSangwan <nikhil.sangwan95@gmail.com>
2016-11-25 22:09:56 +0530

    1st commit

---
+++ controllers/appController.js
@@ -0,0 +1,40 @@
+angular.module('myApp')
+
+	.factory('userDataFactory', ['$http', function($http) {
+		return {
+			userData: function() {
+				var config = {
+					headers : {
+						'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'        	
+                	}
+                }
+                return $http.get('json/data.json');
+			}
+		}
+	}])
+
+	.controller('nav-ctrl', ['$scope', 'userDataFactory', function($scope, userDataFactory) {
+		userDataFactory.userData()
+			.success(function(resp) {
+				$scope.info = resp;
+			});
+	}])
+	.controller('general-ctrl', ['$scope', 'userDataFactory', function($scope, userDataFactory) {
+		userDataFactory.userData()
+			.success(function(resp) {
+				$scope.info = resp;
+			});
+	}])
+	.controller('academics-ctrl', ['$scope', 'userDataFactory', function($scope, userDataFactory) {
+		userDataFactory.userData()
+			.success(function(resp) {
+				$scope.info = resp;
+			});
+		
+	}])
+	.controller('extra-ctrl', ['$scope', 'userDataFactory', function($scope, userDataFactory) {
+		userDataFactory.userData()
+			.success(function(resp) {
+				$scope.info = resp;
+			});
+	}])
---
+++ controllers/mainController.js
@@ -0,0 +1,19 @@
+var app = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMdIcons']);
+
+	app.config([
+		'$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+			$locationProvider.html5Mode(false);
+			$routeProvider.when('/general', {
+				templateUrl: 'templates/general.html',
+				controller: 'general-ctrl'
+			}).when('/academics', {
+				templateUrl: 'templates/academics.html',
+				controller: 'academics-ctrl'
+			}).when('/extra', {
+				templateUrl: 'templates/extra.html',
+				controller: 'extra-ctrl'
+			}).otherwise({
+				redirectTo: '/general'
+			});
+		}
+		]);

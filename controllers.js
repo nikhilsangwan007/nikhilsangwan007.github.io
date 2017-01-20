480224a0ce944f4ebd66cd99bbe7ac87814ea11f
nikhilsangwan007 <nikhil.sangwan007@gmail.com>
2017-01-20 11:58:27 +0530

    adding json and othershit

--- controllers.js
+++ controllers.js
@@ -1,5 +1,22 @@
 angular.module('app', [])
 
-	.controller('appController', ['$scope', function($scope) {
-		$scope.name = "Nikhil Sangwan";
+	.factory('dataFactory', ["$http", function($http) {
+		return {
+			userData : function() {
+				var config = {
+					headers : {
+						'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+					}
+				}
+				return $http.get('json/data.json');
+			}
+		}
 	}])
+
+	.controller('appController', ['$scope', 'dataFactory', function($scope, dataFactory) {
+		dataFactory.userData()
+			.success(function(resp) {
+				$scope.info = resp;
+				console.log($scope.info);
+			});
+	}]);
